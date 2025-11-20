ec4fdb4195fb7151900e27f8952af8b90ee0d277
Carlotta Hoelzle <carlotta.hoelzle@tum.de>
2025-11-20 13:47:49 -0500

    Fix routing: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 // import NotFound from "./pages/NotFound";
 
@@ -16,17 +15,16 @@ const TestNotFound = () => (
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    {/* <TooltipProvider> */}
-      {/* <Toaster /> */}
-      {/* <Sonner /> */}
-      <BrowserRouter>
+    <TooltipProvider>
+      <Toaster />
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<TestNotFound />} />
         </Routes>
-      </BrowserRouter>
-    {/* </TooltipProvider> */}
+      </HashRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
